4c5b15e3a2fa2ad664a4701e488d14d6e636c24d
markb500 <81427572+markb500@users.noreply.github.com>
2025-04-03 14:17:30 +0100

    Add files via upload

--- sincosgraph_module.js
+++ sincosgraph_module.js
@@ -1,7 +1,7 @@
 var sumarrsincos = [];
 function sincosgraph(ctx2) {
   let sum, sumq = "", suma = "", ang1, ang2, trigRatio, sin, cos, tan;
-  sumarrsincos = QLimitRepeats(sumarrsincos, 40);   //Ensures no repeat question until at least 50% of questions shown
+  sumarrsincos = QLimitRepeats(sumarrsincos, 29);   //Ensures no repeat question until at least 50% of questions shown
   sum = sumarrsincos[sumarrsincos.length - 1];
   // sum = 40;
   switch(sum) {
@@ -22,118 +22,58 @@ function sincosgraph(ctx2) {
       ctx2.drawImage(yminus2sinx, 0, 0, 600, 600);
       break;
     case 5:
-      sumq += "Sketch the graph of y = 2Sin(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(y2sin1_5x, 0, 0, 600, 600);
-      break;
-    case 6:
-      sumq += "Sketch the graph of y = -2Sin(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminus2sin1_5x, 0, 0, 600, 600);
-      break;
-    case 7:
-      sumq += "Sketch the graph of y = 2Sin(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(y2sin2x, 0, 0, 600, 600);
-      break;
-    case 8:
-      sumq += "Sketch the graph of y = -2Sin(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminus2sin2x, 0, 0, 600, 600);
-      break;
-    case 9:
-      sumq += "Sketch the graph of y = Sin(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(ysin1_5x, 0, 0, 600, 600);
-      break;
-    case 10:
-      sumq += "Sketch the graph of y = -Sin(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminussin1_5x, 0, 0, 600, 600);
-      break;
-    case 11:
-      sumq += "Sketch the graph of y = Sin(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(ysin2x, 0, 0, 600, 600);
-      break;
-    case 12:
-      sumq += "Sketch the graph of y = -Sin(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminussin2x, 0, 0, 600, 600);
-      break;
-    case 13:
       sumq += "Sketch the graph of y = 1.5Sin(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(y1_5sinx, 0, 0, 600, 600);
       break;
-    case 14:
+    case 6:
       sumq += "Sketch the graph of y = -1.5Sin(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(yminus1_5sinx, 0, 0, 600, 600);
       break;
-    case 15:
+    case 7:
       sumq += "Sketch the graph of y = 0.75Sin(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(y0_75sinx, 0, 0, 600, 600);
       break;
-    case 16:
+    case 8:
       sumq += "Sketch the graph of y = -0.75Sin(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(yminus0_75sinx, 0, 0, 600, 600);
       break;
-    case 17:
+    case 9:
       sumq += "Sketch the graph of y = Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(ycosx, 0, 0, 600, 600);
       break;
-    case 18:
+    case 10:
       sumq += "Sketch the graph of y = -Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(yminuscosx, 0, 0, 600, 600);
       break;
-    case 19:
+    case 11:
       sumq += "Sketch the graph of y = 2Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(y2cosx, 0, 0, 600, 600);
       break;
-    case 20:
+    case 12:
       sumq += "Sketch the graph of y = -2Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(yminus2cosx, 0, 0, 600, 600);
       break;
-    case 21:
-      sumq += "Sketch the graph of y = 2Cos(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(y2cos1_5x, 0, 0, 600, 600);
-      break;
-    case 22:
-      sumq += "Sketch the graph of y = -2Cos(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminus2cos1_5x, 0, 0, 600, 600);
-      break;
-    case 23:
-      sumq += "Sketch the graph of y = 2Cos(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(y2cos2x, 0, 0, 600, 600);
-      break;
-    case 24:
-      sumq += "Sketch the graph of y = -2Cos(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminus2cos2x, 0, 0, 600, 600);
-      break;
-    case 25:
-      sumq += "Sketch the graph of y = Cos(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(ycos1_5x, 0, 0, 600, 600);
-      break;
-    case 26:
-      sumq += "Sketch the graph of y = -Cos(1.5&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminuscos1_5x, 0, 0, 600, 600);
-      break;
-    case 27:
-      sumq += "Sketch the graph of y = Cos(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(ycos2x, 0, 0, 600, 600);
-      break;
-    case 28:
-      sumq += "Sketch the graph of y = -Cos(2&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
-      ctx2.drawImage(yminuscos2x, 0, 0, 600, 600);
-      break;
-    case 29:
+    case 13:
       sumq += "Sketch the graph of y = 1.5Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(y1_5cosx, 0, 0, 600, 600);
       break;
-    case 30:
+    case 14:
       sumq += "Sketch the graph of y = -1.5Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(yminus1_5cosx, 0, 0, 600, 600);
       break;
-    case 31:
+    case 15:
       sumq += "Sketch the graph of y = 0.75Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(y0_75cosx, 0, 0, 600, 600);
       break;
-    case 32:
+    case 16:
       sumq += "Sketch the graph of y = -0.75Cos(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
       ctx2.drawImage(yminus0_75cosx, 0, 0, 600, 600);
       break;
-    case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40:  //Examples of finding all angles 0 to 360 using CAST
+      case 17:
+        sumq += "Sketch the graph of y = Tan(&theta;) from 0<sup>O</sup> to 360<sup>O</sup>";
+        ctx2.drawImage(ytanx, 0, 0, 600, 600);
+        break;
+    case 18: case 19: case 20: case 21: case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29:  //Examples of finding all angles 0 to 360 using CAST
       ctx2.drawImage(CAST, 0, 0, 600, 600);
       do {
         ang1 = rndgen(0, 360, 0, 5, -1);
